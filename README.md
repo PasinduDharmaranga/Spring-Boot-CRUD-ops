351e5bd43899223820f0762b7cea4df4d0fe51e2
unknown <jaffnacsc@gmail.com>
2020-06-15 05:43:23 +0530

    Changes in readme and indexController

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Spring-Boot-CRUD-ops
+This project uses spring boot 1.2, Java 8 and MySQL 8, Maven 3. And you will need Postman for testing requests.
+
+First
+
+Create a database a MySQL 'roles_app' without commas.
+Come the project and change the user and the password for localhost in application.properties according to machine's localhost.
+Open the project folder in eclipse and run RoleApplication.java. Then the application will run on port 8080.
+
+Then open Postman and test the following CRUD operations.
+
+http://localhost:8080/api/role/       This GET request will all the roles.
+http://localhost:8080/api/role/{nic}/{organization}    This GET request will roles by NIC and organization.
+http://localhost:8080/api/role/{nic}   This GET request will roles by NIC. 
+http://localhost:8080/api/role       This POST request will create a new role. 
+http://localhost:8080/api/role/{id}  This PUT request will update an existing role.
+http://localhost:8080/api/role/{id	 This DELETE request will delete and existing role
+
+The API documentation can be found at http://localhost:8080/swagger-ui.html#/role-controller
+
+Recommend to start testing with POST request and create some entities before run other requests
--- Spring-Boot-CRUD-ops/src/main/java/com/example/role/controller/IndexController.java
+++ Spring-Boot-CRUD-ops/src/main/java/com/example/role/controller/IndexController.java
@@ -11,6 +11,25 @@ public class IndexController {
 
     @GetMapping
     public String sayHello() {
-        return "Hello and Welcome to the EasyNotes application. You can create a new Note by making a POST request to /api/notes endpoint.";
+        return "This project uses spring boot 1.2, Java 8 and MySQL 8, Maven 3. And you will need Postman for testing requests.\r\n" + 
+        		"\r\n" + 
+        		"First\r\n" + 
+        		"\r\n" + 
+        		"Create a database a MySQL 'roles_app' without commas.\r\n" + 
+        		"Come the project and change the user and the password for localhost in application.properties according to machine's localhost.\r\n" + 
+        		"Open the project folder in eclipse and run RoleApplication.java. Then the application will run on port 8080.\r\n" + 
+        		"\r\n" + 
+        		"Then open Postman and test the following CRUD operations.\r\n" + 
+        		"\r\n" + 
+        		"http://localhost:8080/api/role/       This GET request will all the roles.\r\n" + 
+        		"http://localhost:8080/api/role/{nic}/{organization}    This GET request will roles by NIC and organization.\r\n" + 
+        		"http://localhost:8080/api/role/{nic}   This GET request will roles by NIC. \r\n" + 
+        		"http://localhost:8080/api/role       This POST request will create a new role. \r\n" + 
+        		"http://localhost:8080/api/role/{id}  This PUT request will update an existing role.\r\n" + 
+        		"http://localhost:8080/api/role/{id	 This DELETE request will delete and existing role\r\n" + 
+        		"\r\n" + 
+        		"The API documentation can be found at http://localhost:8080/swagger-ui.html#/role-controller\r\n" + 
+        		"\r\n" + 
+        		"Recommend to start testing with POST request and create some entities before run other requests";
     }
 }
